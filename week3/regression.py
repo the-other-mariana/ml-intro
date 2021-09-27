efd67359bef1c3da917bde8b8cb71a9435ce2a90
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-09-26 20:12:22 -0500

    update: python version reads a training data file
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- week3/regression.py
+++ week3/regression.py
@@ -1,14 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = [9, 12, 24, 45, 10.5]
-y = [1200, 1520, 2300, 3400, 1370]
+# example training data from file
+f = open("dataset.txt", "r")
+lines = f.readlines()
+x = [float(line.split(',')[0]) for line in lines]
+y = [float(line.split(',')[1]) for line in lines]
+
+# example training data
+#x = [9, 12, 24, 45, 10.5]
+#y = [1200, 1520, 2300, 3400, 1370]
 
 fig = plt.figure()
 fig.add_subplot()
 ax1 = plt.gca()
 
-ax1.scatter(x, y, s=100, marker="o", linewidths=2)
+ax1.scatter(x, y, s=100, color='yellow',marker="o", linewidths=1, edgecolor='black')
 
 a0 = 0.0
 a1 = 0.0
@@ -38,7 +45,9 @@ while iter <= iterMax:
     iter += 1
 
 ax1.plot(x, h, color='g')
-ax1.set_title('Linear Regression')
+ax1.set_title('Univariate Linear Regression')
+ax1.set_ylabel("Price")
+ax1.set_xlabel("Area")
 
 fig2 = plt.figure()
 fig2.add_subplot()
@@ -46,14 +55,16 @@ ax2 = plt.gca()
 
 # for convergence plot
 min = 0.0
-max = 1000.0
+max = iterMax * 1.0
 xs = list(np.arange(min, max, max / len(conv)))
 ax2.plot(xs, conv, color='orange')
 ax2.set_title('Error Convergence')
+ax2.set_ylabel("Error (J)")
+ax2.set_xlabel("Iterations")
 
 # plot test data and output
 input_data = 20
 output_h = a0 + a1 * input_data
-ax1.scatter([input_data], [output_h], marker='o', color='r', s=100, linewidths=2, zorder=100)
+ax1.scatter([input_data], [output_h], marker='o', color='magenta', s=100, linewidths=1, zorder=100, edgecolor='black')
 
 plt.show()
