b4a8d486d316519d2370c9507b72c0c327ebd6b6
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-12-12 09:59:35 -0600

    multiclass nn done
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- week14/multiclass.m
+++ week14/multiclass.m
@@ -1,5 +1,6 @@
-function [W1, W2] = multiclass(W1, W2, X, D, max_epoch)
+function [W1, W2, conv] = multiclass(W1, W2, X, D, max_epoch)
   global m
+  alpha = 0.1;
   for epoch=1:max_epoch
     % each training data
     for i=1:m
@@ -9,6 +10,21 @@ function [W1, W2] = multiclass(W1, W2, X, D, max_epoch)
       % next layer output v
       v = W2 * y1;
       y = softmax(v);
+      % retropropagation
+      e = D(i,:)'-y;
+      delta = e;
+      % vector of errors of size m (for each training data point)
+      error(i) = sum(abs(e));
+      
+      e1 = W2'*delta;
+      delta1 = y1.*(1 - y1).*e1;
+      % delta rule: learning rule
+      dW1 = alpha*delta1*X(i,:);
+      W1 = W1 + dW1;
+      dW2 = alpha*delta*y1';
+      W2 = W2 + dW2;
     end
+    % conv: vector of error per epoch
+    conv(epoch) = sum(error);
   end
 end
--- week14/multipleClassification.m
+++ week14/multipleClassification.m
@@ -7,10 +7,12 @@ data = xlsread('dataset_multiclassOK.xlsx');
 [m,n] = size(data);
 
 % separate x from y
-X = (:1, n-1);
-Y = (:, n);
+X = data(:,1:n-1);
+Y = data(:, n);
 
 % normalize data
+mean_x = mean(X);
+sigma = std(X);
 X = zscore(X);
 
 % get the number of classes
@@ -23,8 +25,49 @@ end
 
 
 W1 = 2*rand((n - 1)*2, n - 1) - 1;
-W1 = 2*rand(numClass, (n - 1)*2) - 1;
+W2 = 2*rand(numClass, (n - 1)*2) - 1;
 
 max_epoch = 100;
 
-[W1, W2] = multiclass(W1, W2, X, D, max_epoch);
+[W1, W2, conv] = multiclass(W1, W2, X, D, max_epoch);
+
+figure(1)
+plot(conv)
+
+% error count
+for i=1:m
+    % inner layer output v1
+    v1 = W1 * X(i,:)';
+    y1 = sigmoid(v1);
+    % next layer output v
+    v = W2 * y1;
+    y(i,:) = softmax(v);
+end
+
+% transform to 1 the column that has > 0.8 probability
+y = y > 0.8;
+% decode columns to a single column with number of class
+for i=1:m
+  for j=1:numClass
+    if y(i,j) == 1
+      classes(i, 1) = j;
+    end
+  end
+end
+
+% total error as difference: count of wrongly classified data
+error = sum(abs(Y - classes))
+% count
+em = y.*D;
+good = sum(sum(em));
+wrong = m - good
+% test
+test_data = [45 2 2 6500 22100];
+x_test_norm = (test_data * mean_x)./sigma;
+
+v_test = W1*x_test_norm';
+y1_test = sigmoid(v_test);
+out = W2*y1_test';
+out = softmax(out)
+
+
